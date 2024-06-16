b28f044ea03359e5e870da76b16d655472e533ef
Yash Chavan <chavanyash127@gmail.com>
2024-06-16 15:27:05 +0530

    Add database MySql for store data of customers

--- Codes/ATM.py
+++ Codes/ATM.py
@@ -1,114 +1,160 @@
-# Main Code -->
-
+import mysql.connector
 import datetime
 import random
-import time 
+import time
+import re
 
-print("\n")
+con = mysql.connector.connect(
+    user='root',
+    host='localhost',
+    database='bank',
+    passwd='Y@sh2081%'
+)
+mycursor = con.cursor()
 
-current_datetime = datetime.datetime.now()
-day_date = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+insert_customer_query = "INSERT INTO customerdata (name, email, gender, pin, bank_acc) VALUES (%s, %s, %s, %s, %s)"
 
 def account_number_generate():
     return str(random.randint(1000, 9999))
 
-class ATM: 
+class ATM:
+    def __init__(self):
+        self.current_balance = 0
+        self.account_number = ''
+        self.acc_holder_name = ''
+        self.email_id = ''
+        self.acc_type = ''
+        self.gender = ''
+        self.customer_gen = ''
+        self.mysql_gen = ''
+        self.pin = ''
+        self.acc_no = 0
 
     def get_email_address(self):
         while True:
-            self.Email_Id = input("$ Email Address: ")
-            if ".com" not in self.Email_Id:
+            email_id = input("$ Email Address: ")
+            if not re.match(r"[^@]+@[^@]+\.[^@]+", email_id):
                 print("$ Invalid Email Address.")
             else:
+                self.email_id = email_id
                 break
 
-    def intial_balance(self):
+    def get_initial_balance(self):
         while True:
-          self.current_balance = float(input("$ Initial Deposit Amount : ")) 
-          if self.current_balance <= 0:
-                print("$ Please Enter valid Initial Deposit Amount")
-          else:
-            break
-          
+            try:
+                initial_balance = float(input("$ Initial Deposit Amount: "))
+                if initial_balance <= 0:
+                    print("$ Please enter a valid Initial Deposit Amount")
+                else:
+                    self.current_balance = initial_balance
+                    break
+            except ValueError:
+                print("$ Please enter a valid number")
+
     def get_account_data(self):
         print("\n$ Please Enter Your Account Details.")
-        self.acc_holder_name = input("$ Account Holder Name : ")
+        self.acc_holder_name = input("$ Account Holder Name: ")
         self.get_email_address()
-        self.acc_type = input("$ Account Type (Saving/Current/Salary) : ") 
-        self.intial_balance()
+        self.acc_type = input("$ Account Type (Saving/Current/Salary): ")
+        self.get_initial_balance()
         while True:
-           self.gender = input("$ Enter Your Gender[M/F] : ")
-           if self.gender.upper() == "MALE" or self.gender.upper() == "M":
-              self.customer_gen = "MR."
-              break
-           elif self.gender.upper() == "FEMALE" or self.gender.upper() == "F":
-              self.customer_gen ="MS."
-              break   
-           else:
-              print("$ Please Enter Valid Gender.")
-              
-        self.pin = input("$ Create 4 Digit PIN : ")    
+            gender = input("$ Enter Your Gender [M/F]: ").upper()
+            if gender == "M" or gender == "MALE":
+                self.customer_gen = "MR."
+                self.mysql_gen = 'M'
+                break
+            elif gender == "F" or gender == "FEMALE":
+                self.customer_gen = "MS."
+                self.mysql_gen = 'F'
+                break
+            else:
+                print("$ Please Enter Valid Gender.")
+        while True:
+            pin = input("$ Create 4 Digit PIN: ")
+            if pin.isdigit() and len(pin) == 4:
+                self.pin = pin
+                break
+            else:
+                print("$ Please enter a valid 4 digit PIN")
 
-    def print_data(self): 
+    def print_data(self):
         time.sleep(0.25)
-        print("\n$ Bank Account Is Created Successfully.") 
+        print("\n$ Bank Account Is Created Successfully.")
         time.sleep(0.25)
         print(f"\n$ {self.acc_type.capitalize()} Bank Account")
-        print(f"$ Customer Name : {self.customer_gen } {self.acc_holder_name.upper()}") 
-        print(f"$ Customer Email Id : {self.Email_Id}")
-        print(f"$ Bank Account No. 3241{self.pin}{self.account_number}") 
-        print("\n") 
-         
-    def withdrawal_paisa(self): 
-        withdrawal_amount = float(input("\n$ Enter Withdrawal Amount From " + self.acc_type.capitalize() + " Account : ")) 
-        if self.current_balance < withdrawal_amount: 
-            print("Insufficient balance! Your available balance is ", self.current_balance) 
-        else: 
-            self.current_balance -= withdrawal_amount 
-            print(f"$ Rs. {withdrawal_amount} Debited from XXXXXXXX{self.account_number} Account On {day_date} .") 
+        print(f"$ Customer Name: {self.customer_gen} {self.acc_holder_name.upper()}")
+        print(f"$ Customer Email Id: {self.email_id}")
+        print(f"$ Bank Account No. 3241{self.pin}{self.account_number}")
 
-    def deposit_paisa(self): 
-        deposit_amount = float(input("\n$ Enter Deposit Amount: ")) 
-        self.current_balance += deposit_amount 
-        print(f"$ Rs. {deposit_amount} Credited To XXXXXXXX{self.account_number} Account On {day_date}.") 
+        self.acc_no = int("3241" + self.pin + self.account_number)
+        customer_data = (self.acc_holder_name, self.email_id, self.mysql_gen, self.pin, self.acc_no)
+        try:
+            mycursor.execute(insert_customer_query, customer_data)
+            con.commit()
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            con.rollback()
+
+        print("\n")
+
+    def withdrawal_paisa(self):
+        try:
+            withdrawal_amount = float(input("\n$ Enter Withdrawal Amount From " + self.acc_type.capitalize() + " Account: "))
+            if self.current_balance < withdrawal_amount:
+                print("Insufficient balance! Your available balance is", self.current_balance)
+            else:
+                self.current_balance -= withdrawal_amount
+                print(f"$ Rs. {withdrawal_amount} Debited from XXXXXXXX{self.account_number} Account On {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.")
+        except ValueError:
+            print("$ Please enter a valid number")
+
+    def deposit_paisa(self):
+        try:
+            deposit_amount = float(input("\n$ Enter Deposit Amount: "))
+            self.current_balance += deposit_amount
+            print(f"$ Rs. {deposit_amount} Credited To XXXXXXXX{self.account_number} Account On {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.")
+        except ValueError:
+            print("$ Please enter a valid number")
+
     def change_pin(self):
         count = 0
         while True:
-            print("\n$ Please Enter Your Old Pin.")
-            old_pin = input("Input Here : ")
+            old_pin = input("\n$ Please Enter Your Old Pin: ")
             if old_pin == self.pin:
-                print("\n$ Please Enter Your New Pin.")
-                new_pin = input("Input Here : ")
-                self.pin = new_pin
-                print("\n$ Your Pin Is Changed Successfully.")
+                while True:
+                    new_pin = input("\n$ Please Enter Your New Pin: ")
+                    if new_pin.isdigit() and len(new_pin) == 4:
+                        self.pin = new_pin
+                        print("\n$ Your Pin Is Changed Successfully.")
+                        break
+                    else:
+                        print("$ Please enter a valid 4 digit PIN")
                 break
             else:
                 count += 1
                 if count == 3:
-                    print("\n$ You Have Entered Wrong Pin 3 Times. Your Account Is Blocked.")                   
-                    print("\n$ Thank You For Using YC Bank ATM. \n\n")
+                    print("\n$ You Have Entered Wrong Pin 3 Times. Your Account Is Blocked.")
+                    print("\n$ Thank You For Using YC Bank ATM.\n\n")
                     exit()
                 else:
-                    print("\n$ Invalid Pin , Please Try Again.")
+                    print("\n$ Invalid Pin, Please Try Again.")
 
     def start(self):
         while True:
-            print("$ Please Press s To Start ATM")
-            start_value = input("Input Here : ")
+            start_value = input("\n$ Please Press s To Start ATM: ")
             if start_value.lower() == "s":
                 print("\n<<--- Welcome To YC Bank ATM --->>")
                 print("\n$ Please Create A New Bank Account.")
-                self.get_account_data() 
-                self.account_number = account_number_generate() 
-                self.print_data() 
+                self.get_account_data()
+                self.account_number = account_number_generate()
+                self.print_data()
                 self.check_pin()
             else:
                 print("$ Invalid Input.")
 
     def check_pin(self):
         while True:
-            print("$ Please Enter Your 4 Digit Pin.")
-            pin_value = input("Input Here : ")
+            pin_value = input("$ Please Enter Your 4 Digit Pin: ")
             if pin_value == self.pin:
                 self.choice()
                 break
@@ -117,12 +163,12 @@ class ATM:
 
     def choice(self):
         while True:
-            print("\n$ Please Enter Your Choice [ Enter Number ex. For Deposit --> 2]\n")
+            print("\n$ Please Enter Your Choice [Enter Number ex. For Deposit --> 2]\n")
             print("1. Withdrawal")
             print("2. Deposit")
             print("3. Check Balance")
             print("4. Change Pin")
-            choice_value = input("\nInput Here : ")
+            choice_value = input("\nInput Here: ")
             if choice_value == "1":
                 self.withdrawal_paisa()
             elif choice_value == "2":
@@ -130,14 +176,14 @@ class ATM:
             elif choice_value == "3":
                 print(f"\n$ Your Current Balance is {self.current_balance}")
             elif choice_value == "4":
-                self.change_pin()    
+                self.change_pin()
             else:
                 print("\n$ Invalid input! Please enter a valid input.")
                 continue
 
             if input("\nDo you want to continue (yes/no)? ").lower() != "yes":
-                print("\n$ Thank You For Using YC Bank ATM. \n\n")
+                print("\n$ Thank You For Using YC Bank ATM.\n\n")
                 exit()
 
-o = ATM() 
-o.start()
+atm = ATM()
+atm.start()
