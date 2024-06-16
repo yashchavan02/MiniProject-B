d658a7add1ebb65e3bb8abfed34a639f2e7294ac
Yash Chavan <chavanyash127@gmail.com>
2024-06-16 15:37:57 +0530

    sql file for customer data

--- ATM.sql
+++ ATM.sql
@@ -1,1 +1,26 @@
 CREATE DATABASE bank;
+
+USE bank;
+
+CREATE TABLE customerdata(
+name VARCHAR(25),
+email VARCHAR(15),
+gender CHAR,
+pin INT,
+bank_acc BIGINT
+);
+
+SELECT * FROM customerdata;
+
+DROP TABLE cusromerdata;
+
+CREATE TABLE customerdata(
+name VARCHAR(30),
+email VARCHAR(30),
+gender CHAR,
+pin INT,
+bank_acc BIGINT
+);
+
+SELECT * FROM customerdata;
+
